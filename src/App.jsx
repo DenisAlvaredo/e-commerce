090fa7ab2a785604b2e48e3c676a46f523102a67
DenisAlvaredo <78327413+DenisAlvaredo@users.noreply.github.com>
2023-07-13 18:24:21 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query';
 import NavBar from './components/navbar'
 import IndexCategories from './components/screens/indexcategories'
 import IndexProducts from './components/screens/indexproducts'
 
+const queryClient = new QueryClient();
+
 function App() {
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+    <NavBar />
       <BrowserRouter>
-        <NavBar />
         <Routes>
           <Route path="/" element={<IndexCategories />} />
           <Route path="/products" element={<IndexProducts />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   )
 }
 
